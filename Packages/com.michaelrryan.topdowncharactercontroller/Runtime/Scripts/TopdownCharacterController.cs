d00ba33de46e2e60d8f60b43dc7bc8374dc63324
Michael R Ryan <c00239510@itcarlow.ie>
2021-11-06 15:27:10 +0000

    Added movement with on input

--- Packages/com.michaelrryan.topdowncharactercontroller/Runtime/Scripts/TopdownCharacterController.cs
+++ Packages/com.michaelrryan.topdowncharactercontroller/Runtime/Scripts/TopdownCharacterController.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class TopdownCharacterController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private float _maxSpeed = 10.0f;
+    public float MaxSpeed { get { return _maxSpeed; } set { _maxSpeed = value; } }
+
+    private Rigidbody2D rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        if (!rb) throw new UnityException(
+            "No Rigidbody2D on TopdownCharacterController " + name + ".");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = GetInputVector() * _maxSpeed;
+    }
+
+    private Vector2 GetInputVector()
+    {
+        Vector2 input = Vector2.zero;
+
+        // Horizontal Input.
+        if (Input.GetKey(KeyCode.LeftArrow))
+            input.x -= 1.0f;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            input.x += 1.0f;
+
+        // Vertical Input.
+        if (Input.GetKey(KeyCode.UpArrow))
+            input.y += 1.0f;
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            input.y -= 1.0f;
+
+        return input;
     }
 }
