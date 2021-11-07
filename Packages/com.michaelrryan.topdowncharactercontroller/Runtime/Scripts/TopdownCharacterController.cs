536b5a3f51767578a7a32ea052b87df836d8eeab
Michael R Ryan <45245378+MichaelRRyan@users.noreply.github.com>
2021-11-07 12:39:45 +0000

    [SC-2] Completed task "movement on input" with tests
    * Added character controller script and prefab
    
    * Added movement with on input
    
    * [SC-2] Completed task - Added tests and made controller testable

---
+++ Packages/com.michaelrryan.topdowncharactercontroller/Runtime/Scripts/TopdownCharacterController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopdownCharacterController : MonoBehaviour
+{
+    [SerializeField]
+    private float _maxSpeed = 10.0f;
+    public float MaxSpeed { get { return _maxSpeed; } set { _maxSpeed = value; } }
+
+    private Rigidbody2D _rb;
+    private Vector2 _frameInput = Vector2.zero;
+    private Vector2 _persistentInput = Vector2.zero;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        if (!_rb) throw new UnityException(
+            "No Rigidbody2D on TopdownCharacterController " + name + ".");
+    }
+
+    public void Update()
+    {
+        GetInput();
+        _rb.velocity = (_persistentInput + _frameInput) * _maxSpeed;
+        _frameInput = Vector2.zero;
+    }
+
+    private void GetInput()
+    {
+        // Horizontal Input.
+        if (Input.GetKey(KeyCode.LeftArrow)) MoveLeft();
+        if (Input.GetKey(KeyCode.RightArrow)) MoveRight();
+
+        // Vertical Input.
+        if (Input.GetKey(KeyCode.UpArrow)) MoveUp();
+        if (Input.GetKey(KeyCode.DownArrow)) MoveDown();
+    }
+
+    public void MoveRight(bool persistent = false)
+    {
+        if (persistent)
+            _persistentInput.x = Mathf.Min(_persistentInput.x + 1.0f, 1.0f);
+        else
+            _frameInput.x = Mathf.Min(_frameInput.x + 1.0f, 1.0f);
+    }
+
+    public void MoveLeft(bool persistent = false)
+    {
+        if (persistent)
+            _persistentInput.x = Mathf.Max(_persistentInput.x - 1.0f, -1.0f);
+        else
+            _frameInput.x = Mathf.Max(_frameInput.x - 1.0f, -1.0f);
+    }
+
+    public void MoveUp(bool persistent = false)
+    {
+        if (persistent)
+            _persistentInput.y = Mathf.Min(_persistentInput.y + 1.0f, 1.0f);
+        else
+            _frameInput.y = Mathf.Min(_frameInput.y + 1.0f, 1.0f);
+    }
+
+    public void MoveDown(bool persistent = false)
+    {
+        if (persistent)
+            _persistentInput.y = Mathf.Max(_persistentInput.y - 1.0f, -1.0f);
+        else
+            _frameInput.y = Mathf.Max(_frameInput.y - 1.0f, -1.0f);
+    }
+
+    public void ClearPersistentInput()
+    {
+        _persistentInput = Vector2.zero;
+    }
+}
---
+++ Packages/com.michaelrryan.topdowncharactercontroller/Tests/Movement.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class Movement
+    {
+        private TopdownCharacterController character;
+
+        [SetUp]
+        public void Setup()
+        {
+            GameObject characterObj = new GameObject();
+            characterObj.AddComponent<TopdownCharacterController>();
+            characterObj.AddComponent<Rigidbody2D>();
+
+            Rigidbody2D rb = characterObj.GetComponent<Rigidbody2D>();
+            rb.isKinematic = true;
+            rb.useFullKinematicContacts = true;
+
+            character =
+                characterObj.GetComponent<TopdownCharacterController>();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            Object.Destroy(character.gameObject);
+        }
+
+        [UnityTest]
+        public IEnumerator HorizontalMovement()
+        {
+            // Rightward movement.
+            Vector3 position = character.transform.position;
+            character.MoveRight(true);
+            yield return new WaitForSeconds(0.5f);
+            Assert.Greater(character.transform.position.x, position.x);
+            character.ClearPersistentInput();
+
+            // Leftward movement.
+            position = character.transform.position;
+            character.MoveLeft(true);
+            yield return new WaitForSeconds(0.5f);
+            Assert.Less(character.transform.position.x, position.x);
+            character.ClearPersistentInput();
+        }
+
+        [UnityTest]
+        public IEnumerator VerticalMovement()
+        {
+            // Upwards movement.
+            Vector3 position = character.transform.position;
+            character.MoveUp(true);
+            yield return new WaitForSeconds(0.5f);
+            Assert.Greater(character.transform.position.y, position.y);
+            character.ClearPersistentInput();
+
+            // Downward movement.
+            position = character.transform.position;
+            character.MoveDown(true);
+            yield return new WaitForSeconds(0.5f);
+            Assert.Less(character.transform.position.y, position.y);
+            character.ClearPersistentInput();
+        }
+
+        [UnityTest]
+        public IEnumerator DiagonalMovement()
+        {
+            // Up + right movement.
+            Vector3 position = character.transform.position;
+            character.MoveUp(true);
+            character.MoveRight(true);
+            yield return new WaitForSeconds(0.5f);
+            Assert.Greater(character.transform.position.y, position.y);
+            Assert.Greater(character.transform.position.x, position.x);
+            character.ClearPersistentInput();
+
+            // Down + left movement.
+            position = character.transform.position;
+            character.MoveDown(true);
+            character.MoveLeft(true);
+            yield return new WaitForSeconds(0.5f);
+            Assert.Less(character.transform.position.y, position.y);
+            Assert.Less(character.transform.position.x, position.x);
+            character.ClearPersistentInput();
+        }
+
+        [UnityTest]
+        public IEnumerator NoMovementOnOppositeInput()
+        {
+            // Vertical movement.
+            Vector3 position = character.transform.position;
+            character.MoveUp(true);
+            character.MoveDown(true);
+            yield return new WaitForSeconds(0.5f);
+            Assert.AreEqual(character.transform.position.y, position.y);
+            character.ClearPersistentInput();
+
+            // Horizontal movement.
+            position = character.transform.position;
+            character.MoveLeft(true);
+            character.MoveRight(true);
+            yield return new WaitForSeconds(0.5f);
+            Assert.AreEqual(character.transform.position.y, position.y);
+            character.ClearPersistentInput();
+        }
+    }
+}
