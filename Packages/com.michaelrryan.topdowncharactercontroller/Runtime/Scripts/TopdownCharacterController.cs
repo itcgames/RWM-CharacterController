f9993f66cc327ad069b0d15490e47bdfb675080a
Michael R Ryan <c00239510@itcarlow.ie>
2021-11-07 12:38:05 +0000

    [SC-2] Completed task - Added tests and made controller testable

--- Packages/com.michaelrryan.topdowncharactercontroller/Runtime/Scripts/TopdownCharacterController.cs
+++ Packages/com.michaelrryan.topdowncharactercontroller/Runtime/Scripts/TopdownCharacterController.cs
@@ -8,38 +8,69 @@ public class TopdownCharacterController : MonoBehaviour
     private float _maxSpeed = 10.0f;
     public float MaxSpeed { get { return _maxSpeed; } set { _maxSpeed = value; } }
 
-    private Rigidbody2D rb;
+    private Rigidbody2D _rb;
+    private Vector2 _frameInput = Vector2.zero;
+    private Vector2 _persistentInput = Vector2.zero;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        if (!rb) throw new UnityException(
+        _rb = GetComponent<Rigidbody2D>();
+        if (!_rb) throw new UnityException(
             "No Rigidbody2D on TopdownCharacterController " + name + ".");
     }
 
-    void Update()
+    public void Update()
     {
-        rb.velocity = GetInputVector() * _maxSpeed;
+        GetInput();
+        _rb.velocity = (_persistentInput + _frameInput) * _maxSpeed;
+        _frameInput = Vector2.zero;
     }
 
-    private Vector2 GetInputVector()
+    private void GetInput()
     {
-        Vector2 input = Vector2.zero;
-
         // Horizontal Input.
-        if (Input.GetKey(KeyCode.LeftArrow))
-            input.x -= 1.0f;
-
-        if (Input.GetKey(KeyCode.RightArrow))
-            input.x += 1.0f;
+        if (Input.GetKey(KeyCode.LeftArrow)) MoveLeft();
+        if (Input.GetKey(KeyCode.RightArrow)) MoveRight();
 
         // Vertical Input.
-        if (Input.GetKey(KeyCode.UpArrow))
-            input.y += 1.0f;
+        if (Input.GetKey(KeyCode.UpArrow)) MoveUp();
+        if (Input.GetKey(KeyCode.DownArrow)) MoveDown();
+    }
 
-        if (Input.GetKey(KeyCode.DownArrow))
-            input.y -= 1.0f;
+    public void MoveRight(bool persistent = false)
+    {
+        if (persistent)
+            _persistentInput.x = Mathf.Min(_persistentInput.x + 1.0f, 1.0f);
+        else
+            _frameInput.x = Mathf.Min(_frameInput.x + 1.0f, 1.0f);
+    }
 
-        return input;
+    public void MoveLeft(bool persistent = false)
+    {
+        if (persistent)
+            _persistentInput.x = Mathf.Max(_persistentInput.x - 1.0f, -1.0f);
+        else
+            _frameInput.x = Mathf.Max(_frameInput.x - 1.0f, -1.0f);
+    }
+
+    public void MoveUp(bool persistent = false)
+    {
+        if (persistent)
+            _persistentInput.y = Mathf.Min(_persistentInput.y + 1.0f, 1.0f);
+        else
+            _frameInput.y = Mathf.Min(_frameInput.y + 1.0f, 1.0f);
+    }
+
+    public void MoveDown(bool persistent = false)
+    {
+        if (persistent)
+            _persistentInput.y = Mathf.Max(_persistentInput.y - 1.0f, -1.0f);
+        else
+            _frameInput.y = Mathf.Max(_frameInput.y - 1.0f, -1.0f);
+    }
+
+    public void ClearPersistentInput()
+    {
+        _persistentInput = Vector2.zero;
     }
 }
