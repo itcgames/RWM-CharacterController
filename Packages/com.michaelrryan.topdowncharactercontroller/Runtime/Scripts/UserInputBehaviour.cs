9fb113f08a494f7939af90339458fe6e34054b34
Michael Rainsford Ryan <C00239510@itcarlow.ie>
2021-11-29 18:40:31 +0000

    Began adding user input behaviour

---
+++ Packages/com.michaelrryan.topdowncharactercontroller/Runtime/Scripts/UserInputBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInputBehaviour : ICharacterBehaviour
+{
+    ICharacterController _characterController;
+    private bool _lastInputWasVertical = false;
+
+    public void SetBehaviourUser(ICharacterController characterController) =>
+            _characterController = characterController;
+
+    public ICharacterController GetBehaviourUser() => _characterController;
+
+    public void Update()
+    {
+        // Horizontal Input.
+        if (Input.GetKey(KeyCode.LeftArrow)) _characterController.MoveLeft();
+        if (Input.GetKey(KeyCode.RightArrow)) _characterController.MoveRight();
+
+        // Vertical Input.
+        if (Input.GetKey(KeyCode.UpArrow)) _characterController.MoveUp();
+        if (Input.GetKey(KeyCode.DownArrow)) _characterController.MoveDown();
+
+        //if (!_diagonalMovement)
+        //{
+        //    // Checks for which direction was pressed last.
+        //    if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
+        //        _lastInputWasVertical = false;
+        //    else if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow))
+        //        _lastInputWasVertical = true;
+        //}
+    }
+}
