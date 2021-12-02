cba1e1407264fe3428aab47e749cfab1bc39fdaa
Michael R Ryan <45245378+MichaelRRyan@users.noreply.github.com>
2021-12-02 18:29:08 +0000

    [SC-7] Feature 'Customisable Behaviours' partially complete (2/3 tasks complete) with tests
    * [SC-8] Completed task 'Slotting in new behaviours' with tests
    
    * [SC-9] Completed task 'User input behaviour' with a test
    
    * Updated package version
    
    * Reset package patch version

---
+++ Packages/com.michaelrryan.topdowncharactercontroller/Runtime/Scripts/ICharacterBehaviour.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICharacterBehaviour
+{
+    void SetBehaviourUser(ICharacterController characterController);
+    ICharacterController GetBehaviourUser();
+    void Update();
+}
