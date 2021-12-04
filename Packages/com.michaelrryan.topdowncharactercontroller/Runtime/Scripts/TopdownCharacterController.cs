cef335237860bf8790abbb96d9827668b46d9cf3
Michael R Ryan <c00239510@itcarlow.ie>
2021-12-04 13:42:59 +0000

    Amended missed merge issues

--- Packages/com.michaelrryan.topdowncharactercontroller/Runtime/Scripts/TopdownCharacterController.cs
+++ Packages/com.michaelrryan.topdowncharactercontroller/Runtime/Scripts/TopdownCharacterController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TopdownCharacterController : MonoBehaviour, ICharacterController
+public class TopdownCharacterController : MonoBehaviour
 {
 	// ==== Properties ====
 
