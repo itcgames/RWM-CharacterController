6d869cc1fd55f7ecdb88650321c33ff5beef9b24
Michael R Ryan <c00239510@itcarlow.ie>
2021-12-11 14:58:37 +0000

    [SC-13] Task complete. Added a test

---
+++ Packages/com.michaelrryan.topdowncharactercontroller/Tests/HealthTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+
+namespace Tests
+{
+    public class HealthTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            SceneManager.LoadScene("DemoScene");
+        }
+
+        [UnityTest]
+        public IEnumerator CharacterDiesOnZeroHealth()
+        {
+            // Gets the character and the script, checking both are not null.
+            GameObject playerObj = GameObject.Find("TopdownCharacter");
+            Assert.NotNull(playerObj);
+            var player = playerObj.GetComponent<TopdownCharacterController>();
+            Assert.NotNull(player);
+
+            // Damages the character equal to its health, waits a frame and
+            //      checks they are now null.
+            player.TakeDamage(player.Health);
+            yield return null;
+            playerObj = GameObject.Find("TopdownCharacter");
+            Assert.Null(playerObj);
+        }
+    }
+}
