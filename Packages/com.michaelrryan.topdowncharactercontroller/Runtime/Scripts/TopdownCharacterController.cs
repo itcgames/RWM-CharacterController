f8a3299be11434f9c9d163925a20f08fa5f2064e
Michael R Ryan <45245378+MichaelRRyan@users.noreply.github.com>
2021-12-12 20:25:14 +0000

    [SC-12] Completed feature 'Health' with tests
    * [SC-13] Completed task 'Character dies on zero health' with a test
    
    * [SC-14] Completed task 'Damage grace period' with tests
    
    * [SC-15] Completed task 'Grace period flashes' with tests
    
    * [SC-16] Completed task 'Damage tags' with tests
    
    * [SC-17] Completed task 'Death callback' with tests

--- Packages/com.michaelrryan.topdowncharactercontroller/Runtime/Scripts/TopdownCharacterController.cs
+++ Packages/com.michaelrryan.topdowncharactercontroller/Runtime/Scripts/TopdownCharacterController.cs
@@ -7,6 +7,32 @@ public class TopdownCharacterController : MonoBehaviour
 {
 	// ==== Properties ====
 
+	[Header("Health & Damage")]
+
+	[SerializeField]
+	private float _health = 5.0f;
+	public float Health { get { return _health; }
+						  set { _health = value; } }
+
+	[SerializeField]
+	private float _damageGracePeriod = 0.8f;
+	public float DamageGracePeriod { get { return _damageGracePeriod; }
+									 set { SetDamageGracePeriod(value); } }
+
+	[SerializeField]
+	private int _gracePeriodFlashes = 4;
+	public int GracePeriodFlashes { get { return _gracePeriodFlashes; }
+									set { _gracePeriodFlashes = value; } }
+
+	[SerializeField]
+	private List<string> _damageWhitelistTags = new List<string>();
+	public List<string> DamageWhitelistTags { get { return _damageWhitelistTags; }
+											  private set { } }
+
+	public delegate void DeathCallback();
+	public List<DeathCallback> DeathCallbacks = new List<DeathCallback>();
+
+	[Header("Movement")]
 	[SerializeField]
 	private bool _tilebasedMovement = false;
 	public bool TilebasedMovement { get { return _tilebasedMovement; } 
@@ -43,10 +69,12 @@ public class TopdownCharacterController : MonoBehaviour
 	// ==== Private Variables ====
 
 	private Rigidbody2D _rb;
+	private Renderer _renderer;
 	private Vector2 _frameInput = Vector2.zero;
 	private Vector2 _persistentInput = Vector2.zero;
 	private float _acceleration = 0.0f;
 	private float _deceleration = 0.0f;
+	private float _lastHitTaken = 0.0f;
 
 	// Used internally for both regular and tilebased movement, similar to
 	//      _diagonalMovementAllowed but can change without a users intention.
@@ -63,6 +91,7 @@ public class TopdownCharacterController : MonoBehaviour
 	private void OnValidate()
 	{
 		// Ensures the properties' set functions are called when one changes.
+		DamageGracePeriod = _damageGracePeriod;
 		TilebasedMovement = _tilebasedMovement;
 		DiagonalMovementAllowed = _diagonalMovementAllowed;
 		TimeToMaxSpeed = _timeToMaxSpeed;
@@ -71,7 +100,8 @@ public class TopdownCharacterController : MonoBehaviour
 
 	private void Start()
 	{
-		_rb = GetComponent<Rigidbody2D>(); // The Rigidbody component.
+		_renderer = GetComponent<Renderer>();
+		_rb = GetComponent<Rigidbody2D>();
 
 		// If no Rigidbody exists, add one.
 		if (!_rb)
@@ -83,6 +113,9 @@ public class TopdownCharacterController : MonoBehaviour
 		// Ensures the rigidbody is set up correctly.
 		_rb.isKinematic = true;
 		_rb.useFullKinematicContacts = true;
+
+		// Sets the last hit taken time so the character can immediately start taking damage.
+		_lastHitTaken = Time.time - _damageGracePeriod * 2.0f;
 	}
 
 	private void Update()
@@ -187,8 +220,32 @@ public class TopdownCharacterController : MonoBehaviour
 		return input;
 	}
 
+	private IEnumerator FlashForGracePeriod()
+	{
+		yield return null;
+
+		// Checks there's a renderer as the character can't flash without it.
+		if (_renderer)
+		{
+			// Works out the number of iterations to flash for.
+			int iterations = _gracePeriodFlashes * 2;
+			for (int i = 0; i < iterations; ++i)
+			{
+				// Toggles the visibility and waits.
+				_renderer.enabled = !_renderer.enabled;
+				yield return new WaitForSeconds(_damageGracePeriod / iterations);
+			}
+		}
+	}
+
 
 	// ==== Setter Methods ====
+
+	private void SetDamageGracePeriod(float value)
+	{
+		_damageGracePeriod = value;
+		_lastHitTaken = Time.time - _damageGracePeriod * 2.0f;
+	}
 
 	private void SetTilebasedMovement(bool value)
 	{
@@ -232,6 +289,30 @@ public class TopdownCharacterController : MonoBehaviour
 	}
 
 	// ==== Public Methods ====
+
+	public void TakeDamage(float damage, string attackersTag = null)
+	{
+		// Checks the grace period has elapsed.
+		if (Time.time >= _lastHitTaken + _damageGracePeriod)
+		{
+			// Checks if the attackers tag is whitelisted to damage this object.
+			if (attackersTag == null || _damageWhitelistTags.Contains(attackersTag))
+			{
+				// Sets the new last hit taken time and takes the damage.
+				_lastHitTaken = Time.time;
+				_health -= damage;
+
+				// Checks if health has reached zero and destroys if so.
+				if (_health <= 0.0f)
+				{
+					foreach (var callback in DeathCallbacks) callback();
+					Destroy(gameObject);
+				}
+				// Starts the flash coroutine if not dead.
+				else StartCoroutine(FlashForGracePeriod());
+			}
+		}
+	}
 
 	public void MoveRight(bool persistent = false)
 	{
---
+++ Packages/com.michaelrryan.topdowncharactercontroller/Tests/HealthTests.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+
+public class HealthTests
+{
+	// Variable and method used in DeathCallbacksAreRunOnDeath().
+	private bool _deathCallbackRun = false;
+
+	void DeathCallback()
+    {
+		_deathCallbackRun = true;
+	}
+
+	[SetUp]
+	public void Setup()
+	{
+		SceneManager.LoadScene("DemoScene");
+	}
+
+	[UnityTest]
+	public IEnumerator CharacterDiesOnZeroHealth()
+	{
+		// Damages the character equal to its health, waits a frame and
+		//      checks they are now null.
+		var character = GetCharacter();
+		character.TakeDamage(character.Health);
+		yield return null;
+
+		GameObject characterObj = GameObject.Find("TopdownCharacter");
+		Assert.Null(characterObj);
+	}
+
+	[UnityTest]
+	public IEnumerator NoDamageTakenDuringGracePeriod()
+	{
+		var character = GetCharacter();
+
+		// Stashes these to avoid interfering with the wait time.
+		float waitTime = character.DamageGracePeriod * 0.25f;
+		float halfHealth = character.Health * 0.5f;
+
+		// Damages the character equal to half its health and stores the
+		//      remaining health.
+		character.TakeDamage(halfHealth);
+		float health = character.Health;
+
+		// Waits half the grace period and tries to attack again.
+		yield return new WaitForSeconds(waitTime);
+		character.TakeDamage(halfHealth);
+
+		// Checks that the health has not changed.
+		Assert.AreEqual(health, character.Health);
+	}
+
+	[UnityTest]
+	public IEnumerator DamageCanBeDoneAfterGracePeriod()
+	{
+		// An added margin to avoid floating point errors.
+		const float ERROR_MARGIN = 0.05f;
+
+		// Damages the character equal to half its health.
+		var character = GetCharacter();
+		character.TakeDamage(character.Health / 2.0f);
+
+		// Waits for the grace period to end and tries to attack again.
+		yield return new WaitForSeconds(character.DamageGracePeriod + ERROR_MARGIN);
+		character.TakeDamage(character.Health);
+
+		// Waits a frame and checks the character is dead.
+		yield return null;
+		GameObject characterObj = GameObject.Find("TopdownCharacter");
+		Assert.Null(characterObj);
+	}
+
+	[UnityTest]
+	public IEnumerator FlashesDuringGracePeriod()
+	{
+		// An added margin to avoid floating point errors.
+		const float ERROR_MARGIN = 0.05f;
+
+		// Sets the properties to be used.
+		float gracePeriod = 1.0f;
+		int flashes = 2;
+
+		// Stash for later to avoid interfering with the wait time.
+		float flashLength = gracePeriod / (flashes * 2);
+		float halfFlash = flashLength * 0.5f;
+
+		// Gets the character and sets up the fields.
+		var character = GetCharacter();
+		character.DamageGracePeriod = gracePeriod;
+		character.GracePeriodFlashes = flashes;
+
+		// Takes reference to the character's renderer and ensures its not null.
+		Renderer renderer = character.GetComponent<Renderer>();
+		Assert.IsNotNull(renderer);
+
+		// Damage the character and checks the character is invisible less than
+		//      one flash length later.
+		character.TakeDamage(character.Health / 2.0f);
+		yield return new WaitForSeconds(halfFlash);
+		Assert.IsFalse(renderer.enabled);
+
+		// Checks the character is visible again after the first flash.
+		yield return new WaitForSeconds(halfFlash + ERROR_MARGIN);
+		Assert.IsTrue(renderer.enabled);
+	}
+
+	[UnityTest]
+	public IEnumerator IsVisibleAfterGracePeriod()
+	{
+		// Gets the character and the renderer.
+		var character = GetCharacter();
+		Renderer renderer = character.GetComponent<Renderer>();
+		Assert.IsNotNull(renderer);
+
+		// Damages the character and checks its visible after the grace period.
+		character.TakeDamage(character.Health / 2.0f);
+		yield return new WaitForSeconds(character.DamageGracePeriod);
+		Assert.IsTrue(renderer.enabled);
+	}
+
+	[UnityTest]
+	public IEnumerator WhitelistedTagsCanDamage()
+	{
+		// Gets the character and whitelists the "Enemy" tag.
+		var character = GetCharacter();
+		character.DamageWhitelistTags.Add("Enemy");
+
+		// Takes a copy for later.
+		float health = character.Health;
+
+		// Damages the character, passing the whitelisted tag.
+		character.TakeDamage(character.Health / 2.0f, "Enemy");
+
+		// Checks the character's health has gone down the next frame.
+		yield return null;
+		Assert.Less(character.Health, health);
+	}
+
+	[UnityTest]
+	public IEnumerator NonWhitelistedTagsCannotDamage()
+	{
+		// Gets the character and whitelists the "Enemy" tag.
+		var character = GetCharacter();
+		character.DamageWhitelistTags.Add("Enemy");
+
+		// Takes a copy for later.
+		float health = character.Health;
+
+		// Damages the character, passing the a non whitelisted tag.
+		character.TakeDamage(character.Health / 2.0f, "Friend");
+
+		// Checks the character's health has not changed.
+		yield return null;
+		Assert.AreEqual(character.Health, health);
+	}
+
+	[UnityTest]
+	public IEnumerator DeathCallbacksAreRunOnDeath()
+	{
+		// Gets the character adds the callback and initialises the checker variable.
+		var character = GetCharacter();
+		character.DeathCallbacks.Add(DeathCallback);
+		_deathCallbackRun = false;
+
+		// Kills the character and checks the callback was run next frame.
+		character.TakeDamage(character.Health);
+		yield return null;
+		Assert.IsTrue(_deathCallbackRun);
+	}
+
+	private TopdownCharacterController GetCharacter()
+	{ 
+		// Gets the character and the script, checking both are not null.
+		GameObject characterObj = GameObject.Find("TopdownCharacter");
+		Assert.NotNull(characterObj);
+		var character = characterObj.GetComponent<TopdownCharacterController>();
+		Assert.NotNull(character);
+
+		return character;
+	}
+}
