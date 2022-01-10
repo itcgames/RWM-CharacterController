3ad6711cfb5145f3ccde185236f6feb0031c5a39
Michael R Ryan <c00239510@itcarlow.ie>
2022-01-10 21:11:04 +0000

    [SC-20] Task completed. Added a test.

--- Packages/com.michaelrryan.topdowncharactercontroller/Tests/MeleeAttackTests.cs
+++ Packages/com.michaelrryan.topdowncharactercontroller/Tests/MeleeAttackTests.cs
@@ -47,4 +47,61 @@ public class MeleeAttackTests
 		// Checks the enemy's health has decreased.
 		Assert.AreEqual(ENEMY_HEALTH - ATTACK_DAMAGE, enemy.Health);
 	}
+
+	[UnityTest]
+	public IEnumerator CannotAttackDuringCooldown()
+	{
+		// SETS UP THE CHARACTERS.
+		const float ATTACK_RADIUS = 1.0f;
+		const float ATTACK_DAMAGE = 1.0f;
+		const float ATTACK_COOLDOWN = 0.5f;
+		const float ENEMY_HEALTH = 3.0f;
+		const float ENEMY_DAMAGE_GRACE_PERIOD = 0.0f;
+
+		// Gets the characters.
+		var character = TestUtilities.GetDefaultCharacter();
+		var enemy = TestUtilities.GetCharacterByName(NPC_NAME);
+
+		// Sets the player and enemy properties.
+		character.AttackRadius = ATTACK_RADIUS;
+		character.AttackDamage = ATTACK_DAMAGE;
+		character.AttackCooldown = ATTACK_COOLDOWN;
+		enemy.Health = ENEMY_HEALTH;
+		enemy.DamageGracePeriod = ENEMY_DAMAGE_GRACE_PERIOD;
+
+		// Makes the character face the right.
+		character.MoveRight();
+		yield return null;
+
+		// Positions the enemy within attack range.
+		enemy.transform.position = character.transform.position
+			+ Vector3.right * ATTACK_RADIUS;
+		yield return null;
+
+
+		// CHECKS THE ATTACK WORKS.
+		// Attacks, waits a frame and checks the enemy's health has decreased.
+		character.Attack();
+		yield return null;
+		Assert.AreEqual(ENEMY_HEALTH - ATTACK_DAMAGE, enemy.Health);
+
+
+		// CHECK THE COOLDOWN STOPS THE ATTACK.
+		// Attacks, waits a frame and checks the enemy's health has not decreased further.
+		character.Attack();
+		yield return null;
+		Assert.AreEqual(ENEMY_HEALTH - ATTACK_DAMAGE, enemy.Health);
+
+
+		// CHECKS THE COOLDOWN HAS EXPIRED.
+		// Waits for the attack cooldown to elapse before attacking again.
+		yield return new WaitForSeconds(ATTACK_COOLDOWN + 0.1f);
+
+		// Attacks and waits a frame.
+		character.Attack();
+		yield return null;
+
+		// Checks the enemy's health has not decreased further.
+		Assert.AreEqual(ENEMY_HEALTH - ATTACK_DAMAGE * 2.0f, enemy.Health);
+	}
 }
