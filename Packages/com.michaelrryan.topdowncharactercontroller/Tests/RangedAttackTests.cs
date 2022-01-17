fd91f73bf92dc1c56d6ac70b28231f3960d86b0d
Michael R Ryan <c00239510@itcarlow.ie>
2022-01-17 12:07:20 +0000

    [SC-24] Task complete. Added a test

---
+++ Packages/com.michaelrryan.topdowncharactercontroller/Tests/RangedAttackTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+public class RangedAttackTests
+{
+    const string PROJECTILE_NAME = "BasicProjectile(Clone)";
+
+    [SetUp]
+    public void Setup()
+    {
+        SceneManager.LoadScene(TestUtilities.GetDefaultSceneName());
+    }
+
+    [UnityTest]
+    public IEnumerator ProjectileFires()
+    {
+        // Gets the player character.
+        TopdownCharacterController player = 
+            TestUtilities.GetDefaultCharacter();
+
+        // Gets the ranged attack component and checks it's not null.
+        TopdownRangedAttack rangedAttack = 
+            player.GetComponent<TopdownRangedAttack>();
+        Assert.NotNull(rangedAttack);
+
+        // Fires a projectile and waits a frame.
+        GameObject fired = rangedAttack.Fire(player.Direction);
+        yield return null;
+
+        // Ensures the projectile exists and matches the projectile returned.
+        GameObject projectile = GameObject.Find(PROJECTILE_NAME);
+        Assert.NotNull(projectile);
+        Assert.AreSame(fired, projectile);
+
+        // Ensures the projectile and player share the same position.
+        Assert.AreEqual(player.transform.position, projectile.transform.position);
+    }
+}
