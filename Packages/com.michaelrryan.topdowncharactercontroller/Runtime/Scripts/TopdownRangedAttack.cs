74cb22148ce1dbccad5c60213a881975c7554bc0
Michael R Ryan <c00239510@itcarlow.ie>
2022-01-17 12:23:48 +0000

    Added ranged attack cooldown variables

--- Packages/com.michaelrryan.topdowncharactercontroller/Runtime/Scripts/TopdownRangedAttack.cs
+++ Packages/com.michaelrryan.topdowncharactercontroller/Runtime/Scripts/TopdownRangedAttack.cs
@@ -6,9 +6,36 @@ public class TopdownRangedAttack : MonoBehaviour
 {
     public GameObject projectilePrefab;
 
+    [SerializeField]
+    private float _cooldown = 0.5f;
+    public float Cooldown { get { return _cooldown; }
+                            set { SetCooldown(value); } }
+
+    private float _lastAttackTime;
+
     public GameObject Fire(Vector2 direction)
     {
         Quaternion rotation = Quaternion.LookRotation(direction, Vector3.back);
         return Instantiate(projectilePrefab, transform.position, rotation);
+    }
+
+    private void Start()
+    {
+        // Ensures no cooldown needs to be waited on when the scene begins.
+        SetCooldown(_cooldown);
+    }
+
+    private void OnValidate()
+    {
+        // Ensures the properties' set functions are called when one changes.
+        Cooldown = _cooldown;
+    }
+
+    private void SetCooldown(float value)
+    {
+        _cooldown = value;
+
+        // Sets the last attack time so no cooldown needs to be waited on.
+        _lastAttackTime = Time.time - _cooldown * 2.0f;
     }
 }
