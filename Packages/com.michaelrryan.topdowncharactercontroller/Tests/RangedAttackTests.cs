d4b9fac60e949f980c727f95b2244bb46db9f5da
Michael R Ryan <c00239510@itcarlow.ie>
2022-01-17 12:34:23 +0000

    [SC-25] Completed task 'Cooldown' with a test

--- Packages/com.michaelrryan.topdowncharactercontroller/Tests/RangedAttackTests.cs
+++ Packages/com.michaelrryan.topdowncharactercontroller/Tests/RangedAttackTests.cs
@@ -7,36 +7,64 @@ using UnityEngine.TestTools;
 
 public class RangedAttackTests
 {
-    const string PROJECTILE_NAME = "BasicProjectile(Clone)";
+	const string PROJECTILE_NAME = "BasicProjectile(Clone)";
 
-    [SetUp]
-    public void Setup()
-    {
-        SceneManager.LoadScene(TestUtilities.GetDefaultSceneName());
-    }
+	[SetUp]
+	public void Setup()
+	{
+		SceneManager.LoadScene(TestUtilities.GetDefaultSceneName());
+	}
 
-    [UnityTest]
-    public IEnumerator ProjectileFires()
-    {
-        // Gets the player character.
-        TopdownCharacterController player = 
-            TestUtilities.GetDefaultCharacter();
+	[UnityTest]
+	public IEnumerator ProjectileFires()
+	{
+		// Gets the player character.
+		TopdownCharacterController player = 
+			TestUtilities.GetDefaultCharacter();
 
-        // Gets the ranged attack component and checks it's not null.
-        TopdownRangedAttack rangedAttack = 
-            player.GetComponent<TopdownRangedAttack>();
-        Assert.NotNull(rangedAttack);
+		// Gets the ranged attack component and checks it's not null.
+		TopdownRangedAttack rangedAttack = 
+			player.GetComponent<TopdownRangedAttack>();
+		Assert.NotNull(rangedAttack);
 
-        // Fires a projectile and waits a frame.
-        GameObject fired = rangedAttack.Fire(player.Direction);
-        yield return null;
+		// Fires a projectile and waits a frame.
+		GameObject fired = rangedAttack.Fire(player.Direction);
+		yield return null;
 
-        // Ensures the projectile exists and matches the projectile returned.
-        GameObject projectile = GameObject.Find(PROJECTILE_NAME);
-        Assert.NotNull(projectile);
-        Assert.AreSame(fired, projectile);
+		// Ensures the projectile exists and matches the projectile returned.
+		GameObject projectile = GameObject.Find(PROJECTILE_NAME);
+		Assert.NotNull(projectile);
+		Assert.AreSame(fired, projectile);
 
-        // Ensures the projectile and player share the same position.
-        Assert.AreEqual(player.transform.position, projectile.transform.position);
-    }
+		// Ensures the projectile and player share the same position.
+		Assert.AreEqual(player.transform.position, projectile.transform.position);
+	}
+
+	[UnityTest]
+	public IEnumerator CannotFireDuringCooldown()
+	{
+		const float SAFETY_MARGIN = 0.1f;
+
+		// Gets the player character.
+		TopdownCharacterController player =
+			TestUtilities.GetDefaultCharacter();
+
+		// Gets the ranged attack component and checks it's not null.
+		TopdownRangedAttack rangedAttack =
+			player.GetComponent<TopdownRangedAttack>();
+		Assert.NotNull(rangedAttack);
+
+		// Fires a projectile and checks it's not null.
+		GameObject fired = rangedAttack.Fire(player.Direction);
+		Assert.NotNull(fired);
+
+		// Tries to fire another projectile and ensures it's null.
+		fired = rangedAttack.Fire(player.Direction);
+		Assert.Null(fired);
+
+		// Waits until the cooldown expires and fire again.
+		yield return new WaitForSeconds(rangedAttack.Cooldown + SAFETY_MARGIN);
+		fired = rangedAttack.Fire(player.Direction);
+		Assert.NotNull(fired);
+	}
 }
