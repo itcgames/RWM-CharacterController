9064f86f9ffa048cfb77db1edb423d952b8f5f4b
Michael R Ryan <c00239510@itcarlow.ie>
2022-01-17 11:55:06 +0000

    Added projectile firing and a basic projectile prefab

---
+++ Packages/com.michaelrryan.topdowncharactercontroller/Runtime/Scripts/TopdownRangedAttack.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopdownRangedAttack : MonoBehaviour
+{
+    public GameObject projectilePrefab;
+
+    public GameObject Fire(Vector2 direction)
+    {
+        Quaternion rotation = Quaternion.LookRotation(direction, Vector3.back);
+        return Instantiate(projectilePrefab, transform.position, rotation);
+    }
+}
