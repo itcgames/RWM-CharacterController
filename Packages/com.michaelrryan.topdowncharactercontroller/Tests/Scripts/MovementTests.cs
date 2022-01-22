14a44c221916810e684520cac55086e280a35cce
Michael R Ryan <c00239510@itcarlow.ie>
2022-01-22 15:16:53 +0000

    [SC-29] Task complete. Fixed movement tests

--- Packages/com.michaelrryan.topdowncharactercontroller/Tests/Scripts/MovementTests.cs
+++ Packages/com.michaelrryan.topdowncharactercontroller/Tests/Scripts/MovementTests.cs
@@ -1,101 +1,100 @@
 ﻿using System.Collections;
 using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 
 public class MovementTests
 {
-    private TopdownCharacterController character;
-
     [SetUp]
     public void Setup()
     {
-        GameObject characterObj = new GameObject();
-        characterObj.AddComponent<TopdownCharacterController>();
-        character = characterObj.GetComponent<TopdownCharacterController>();
+        SceneManager.LoadScene(TestUtilities.GetDefaultSceneName());
     }
-
-    [TearDown]
-    public void Teardown()
-    {
-        Object.Destroy(character.gameObject);
-    }
-
 
     // ==== Movement On Input Tests ====
 
     [UnityTest]
     public IEnumerator HorizontalMovement()
     {
+        var player = TestUtilities.GetDefaultCharacter();
+
         // Rightward movement.
-        Vector3 position = character.transform.position;
-        character.MoveRight(true);
+        Vector3 position = player.transform.position;
+        player.MoveRight(true);
         yield return new WaitForSeconds(0.5f);
-        Assert.Greater(character.transform.position.x, position.x);
-        character.ClearPersistentInput();
+        Assert.Greater(player.transform.position.x, position.x);
+        player.ClearPersistentInput();
 
         // Leftward movement.
-        position = character.transform.position;
-        character.MoveLeft(true);
+        position = player.transform.position;
+        player.MoveLeft(true);
         yield return new WaitForSeconds(0.5f);
-        Assert.Less(character.transform.position.x, position.x);
+        Assert.Less(player.transform.position.x, position.x);
     }
 
     [UnityTest]
     public IEnumerator VerticalMovement()
     {
+        var player = TestUtilities.GetDefaultCharacter();
+
         // Upwards movement.
-        Vector3 position = character.transform.position;
-        character.MoveUp(true);
+        Vector3 position = player.transform.position;
+        player.MoveUp(true);
         yield return new WaitForSeconds(0.5f);
-        Assert.Greater(character.transform.position.y, position.y);
-        character.ClearPersistentInput();
+        Assert.Greater(player.transform.position.y, position.y);
+        player.ClearPersistentInput();
 
         // Downward movement.
-        position = character.transform.position;
-        character.MoveDown(true);
+        position = player.transform.position;
+        player.MoveDown(true);
         yield return new WaitForSeconds(0.5f);
-        Assert.Less(character.transform.position.y, position.y);
+        Assert.Less(player.transform.position.y, position.y);
     }
 
     [UnityTest]
     public IEnumerator DiagonalMovement()
     {
+        var player = TestUtilities.GetDefaultCharacter();
+        player.DiagonalMovementAllowed = true;
+
         // Up + right movement.
-        Vector3 position = character.transform.position;
-        character.MoveUp(true);
-        character.MoveRight(true);
+        Vector3 position = player.transform.position;
+        player.MoveUp(true);
+        player.MoveRight(true);
         yield return new WaitForSeconds(0.5f);
-        Assert.Greater(character.transform.position.y, position.y);
-        Assert.Greater(character.transform.position.x, position.x);
-        character.ClearPersistentInput();
+        Assert.Greater(player.transform.position.y, position.y);
+        Assert.Greater(player.transform.position.x, position.x);
+        player.ClearPersistentInput();
 
         // Down + left movement.
-        position = character.transform.position;
-        character.MoveDown(true);
-        character.MoveLeft(true);
+        position = player.transform.position;
+        player.MoveDown(true);
+        player.MoveLeft(true);
         yield return new WaitForSeconds(0.5f);
-        Assert.Less(character.transform.position.y, position.y);
-        Assert.Less(character.transform.position.x, position.x);
+        Assert.Less(player.transform.position.y, position.y);
+        Assert.Less(player.transform.position.x, position.x);
     }
 
     [UnityTest]
     public IEnumerator NoMovementOnOppositeInput()
     {
+        var player = TestUtilities.GetDefaultCharacter();
+
         // Vertical movement.
-        Vector3 position = character.transform.position;
-        character.MoveUp(true);
-        character.MoveDown(true);
+        Vector3 position = player.transform.position;
+        player.MoveUp(true);
+        player.MoveDown(true);
         yield return new WaitForSeconds(0.5f);
-        Assert.AreEqual(character.transform.position.y, position.y);
-        character.ClearPersistentInput();
+        Assert.AreEqual(player.transform.position.y, position.y);
+        player.ClearPersistentInput();
 
         // Horizontal movement.
-        position = character.transform.position;
-        character.MoveLeft(true);
-        character.MoveRight(true);
+        position = player.transform.position;
+        player.MoveLeft(true);
+        player.MoveRight(true);
         yield return new WaitForSeconds(0.5f);
-        Assert.AreEqual(character.transform.position.y, position.y);
+        Assert.AreEqual(player.transform.position.y, position.y);
     }
 
 
@@ -104,27 +103,29 @@ public class MovementTests
     [UnityTest]
     public IEnumerator NoDiagonalMovementWhenDisabled()
     {
+        var player = TestUtilities.GetDefaultCharacter();
+
         // Giving these properties the following values, the character should
         //      only move horizontally when moving vertically and horizontally.
-        character.DiagonalMovementAllowed = false;
-        character.PreferHorizontal = true;
+        player.DiagonalMovementAllowed = false;
+        player.PreferHorizontal = true;
 
         // Up + right movement.
-        Vector3 position = character.transform.position;
-        character.MoveUp(true);
-        character.MoveRight(true);
+        Vector3 position = player.transform.position;
+        player.MoveUp(true);
+        player.MoveRight(true);
         yield return new WaitForSeconds(0.5f);
-        Assert.AreEqual(character.transform.position.y, position.y);
-        Assert.Greater(character.transform.position.x, position.x);
-        character.ClearPersistentInput();
+        Assert.AreEqual(player.transform.position.y, position.y);
+        Assert.Greater(player.transform.position.x, position.x);
+        player.ClearPersistentInput();
 
         // Down + left movement.
-        position = character.transform.position;
-        character.MoveDown(true);
-        character.MoveLeft(true);
+        position = player.transform.position;
+        player.MoveDown(true);
+        player.MoveLeft(true);
         yield return new WaitForSeconds(0.5f);
-        Assert.AreEqual(character.transform.position.y, position.y);
-        Assert.Less(character.transform.position.x, position.x);
+        Assert.AreEqual(player.transform.position.y, position.y);
+        Assert.Less(player.transform.position.x, position.x);
     }
 
         
@@ -132,60 +133,66 @@ public class MovementTests
     [UnityTest]
     public IEnumerator TilebasedMovement()
     {
+        var player = TestUtilities.GetDefaultCharacter();
+
         // A small buffer to allow for timing based issues in the test.
         const float TimeErrorBuffer = 0.01f;
 
         // Sets up the character for tile based movement.
-        character.TilebasedMovement = true;
-        character.SecondsPerTile = 0.75f;
-        character.TileSize = 1.27f;
+        player.TilebasedMovement = true;
+        player.SecondsPerTile = 0.75f;
+        player.TileSize = 1.27f;
 
         // Works out the character's position after moving one tile.
-        Vector3 destination = character.transform.position 
-            + Vector3.right * character.TileSize;
+        Vector3 destination = player.transform.position 
+            + Vector3.right * player.TileSize;
 
         // Checks the character hasn't reached the destination halfway
         //      through the time.
-        character.MoveRight();
+        player.MoveRight();
         // Waits half the movement time.
-        yield return new WaitForSeconds(character.SecondsPerTile / 2.0f); 
-        Assert.AreNotEqual(character.transform.position, destination);
+        yield return new WaitForSeconds(player.SecondsPerTile / 2.0f); 
+        Assert.AreNotEqual(player.transform.position, destination);
 
         // Waits another half the movement time plus an error buffer and
         //      checks the character is at the destination.
-        yield return new WaitForSeconds(character.SecondsPerTile / 2.0f 
+        yield return new WaitForSeconds(player.SecondsPerTile / 2.0f 
             + TimeErrorBuffer);
-        Assert.AreEqual(character.transform.position, destination);
+        Assert.AreEqual(player.transform.position, destination);
     }
 
     // ==== Acceleration Test ====
     [UnityTest]
     public IEnumerator Acceleration()
     {
-        character.TimeToMaxSpeed = 0.5f;
+        var player = TestUtilities.GetDefaultCharacter();
 
-        character.MoveRight(true);
-        yield return new WaitForSeconds(character.TimeToMaxSpeed / 2.0f);
-        Assert.AreNotEqual(character.GetSpeed(), character.MaxSpeed);
-        yield return new WaitForSeconds(character.TimeToMaxSpeed / 2.0f);
-        Assert.AreEqual(character.GetSpeed(), character.MaxSpeed);
+        player.TimeToMaxSpeed = 0.5f;
+
+        player.MoveRight(true);
+        yield return new WaitForSeconds(player.TimeToMaxSpeed / 2.0f);
+        Assert.AreNotEqual(player.GetSpeed(), player.MaxSpeed);
+        yield return new WaitForSeconds(player.TimeToMaxSpeed / 2.0f);
+        Assert.AreEqual(player.GetSpeed(), player.MaxSpeed);
     }
 
     // ==== Deceleration Test ====
     [UnityTest]
     public IEnumerator Deceleration()
     {
-        character.TimeToFullStop = 0.5f;
+        var player = TestUtilities.GetDefaultCharacter();
 
-        character.MoveRight(true);
+        player.TimeToFullStop = 0.5f;
+
+        player.MoveLeft(true);
         yield return new WaitForSeconds(0.5f);
-        Assert.AreEqual(character.GetSpeed(), character.MaxSpeed);
+        Assert.AreEqual(player.GetSpeed(), player.MaxSpeed);
 
-        character.ClearPersistentInput();
-        yield return new WaitForSeconds(character.TimeToFullStop / 2.0f);
-        Assert.AreNotEqual(character.GetSpeed(), 0.0f);
+        player.ClearPersistentInput();
+        yield return new WaitForSeconds(player.TimeToFullStop / 2.0f);
+        Assert.AreNotEqual(0.0f, player.GetSpeed());
 
-        yield return new WaitForSeconds(character.TimeToFullStop / 2.0f);
-        Assert.AreEqual(character.GetSpeed(), 0.0f);
+        yield return new WaitForSeconds(player.TimeToFullStop / 2.0f);
+        Assert.AreEqual(0.0f, player.GetSpeed());
     }
 }
